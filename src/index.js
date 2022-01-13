56e6845d2692876c1581a83da7ac4806f91549fd
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2022-01-13 15:27:51 +0200

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-      <App />
-    {/* </BrowserRouter> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
